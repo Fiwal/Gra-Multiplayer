11452d272e2f5e81a5c05eab1229078903dfb6d8
Python <fiwalczowski@gmail.com>
2023-04-18 19:33:51 +0200

    new version

--- network.py
+++ network.py
@@ -25,6 +25,3 @@ class Network:
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
-
-
-n = Network()
--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 import socket
 from _thread import *
-import sys
+
+
+def read_pos(text):
+    text = text.split(",")
+    return int(text[0]), int(text[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
 
 server = "192.168.1.85"
 port = 5555
@@ -17,30 +25,43 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, server Started")
 
-def threaded_client(conn):
-    conn.send(str.encode("Connected"))
+positions = [(0, 0), (100, 100)]
+
+def threaded_client(conn, playerNum):
+    conn.send(str.encode(make_pos(positions[playerNum])))
+    print(str.encode(make_pos(positions[playerNum])))
     reply = ""
     while True:
+
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            positions[playerNum] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
+                if playerNum == 1:
+                    reply = positions[0]
+                else:
+                    reply = positions[1]
+
                 print(f"Received: {reply}")
                 print(f"Sending: {reply}")
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
+
         except:
             break
+
     print("Lost connection")
     conn.close()
 
+currentPlayer = 0
 
 while True:
     conn, addr = s.accept()
     print(f"Connected to: {addr}.")
 
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
